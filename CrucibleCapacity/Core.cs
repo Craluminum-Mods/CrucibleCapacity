18916ff675c3fa1455cf0c9ef328bbe20d470683
Dana <69315569+Craluminum2413@users.noreply.github.com>
2025-09-15 20:44:05 +0300

    rename modsystem

---
+++ CrucibleCapacity/Core.cs
@@ -0,0 +1,31 @@
+using Vintagestory.API.Common;
+
+namespace CrucibleCapacity;
+
+public class Core : ModSystem
+{
+	public override void StartPre(ICoreAPI api)
+	{
+		try
+		{
+			CrucibleCapacityConfig FromDisk;
+			if ((FromDisk = api.LoadModConfig<CrucibleCapacityConfig>("CrucibleCapacityConfig.json")) == null)
+			{
+				api.StoreModConfig(CrucibleCapacityConfig.Loaded, "CrucibleCapacityConfig.json");
+			}
+			else CrucibleCapacityConfig.Loaded = FromDisk;
+		}
+		catch
+		{
+			api.StoreModConfig(CrucibleCapacityConfig.Loaded, "CrucibleCapacityConfig.json");
+		}
+
+	  api.World.Config.SetInt($"CrucibleCapacityPerSlot", CrucibleCapacityConfig.Loaded.CrucibleCapacityPerSlot);
+	}
+
+	public class CrucibleCapacityConfig : ModSystem
+	{
+	  public static CrucibleCapacityConfig Loaded { get; set; } = new CrucibleCapacityConfig();
+	  public int CrucibleCapacityPerSlot { get; set; } = 15;
+	}
+}
